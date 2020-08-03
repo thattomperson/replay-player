2bbce4e9fd92df3e05491f15afd47e6d8934b16d
Tom Albrighton <thomas.albrighton@four.io>
2020-08-03 23:00:00 +0930

    Cache doesn't work in vercel

--- api/replays/[id]/replay.ts
+++ api/replays/[id]/replay.ts
@@ -3,7 +3,7 @@ import axios from '../../../util/axios';
 import levelup from 'levelup'
 import leveldown from 'leveldown'
  
-var db = levelup(leveldown('./node_modules/.cache'))
+// var db = levelup(leveldown('./node_modules/.cache'))
 
 
 const client = axios.create({
@@ -15,17 +15,17 @@ type Response = string
 
 export default async (req: NowRequest, res: NowResponse) => {
   let data
-  try {
-    data = await db.get(req.query.id)
-  } catch (err) {
-    if (err.notFound) {
-      console.log('refreshing data')
+  // try {
+  //   data = await db.get(req.query.id)
+  // } catch (err) {
+  //   if (err.notFound) {
+  //     console.log('refreshing data')
       const response = await client.get<Response>(`/dyn/replay/${req.query.id}/threejs`)
       const l = 'var replayData='.length
       data = response.data.substr(l, response.data.lastIndexOf(';') - l)  
-      await db.put(req.query.id, data);
-    }
-  } finally {
+  //     await db.put(req.query.id, data);
+  //   }
+  // } finally {
     res.json(JSON.parse(data))
-  }
+  // }
 }
