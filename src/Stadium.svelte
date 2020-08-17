980e701b498bce4d47f11e361b401e31f58a1111
Tom Albrighton <thomas.albrighton@four.io>
2020-08-17 14:52:39 +0930

    Added goals

--- src/Stadium.svelte
+++ src/Stadium.svelte
@@ -4,7 +4,7 @@
   <!-- Ground -->
   <GL.Mesh
     geometry={GL.plane()}
-    location={[0, 0, 0]}
+    location={[0, 0, FLOOR_HEIGHT]}
     scale={[SOCCAR_XSIZE, SOCCAR_YSIZE, 1]}
     uniforms={{ color: 0xc0c0c0 }}
     transparent={true}
@@ -54,6 +54,51 @@
     uniforms={{ color: 0xffc0c0 }}
     transparent={true}
   />
+
+
+  <GL.Group>
+    <!-- Goal Floor -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[0, SOCCAR_YSIZE + GOAL_DEPTH, FLOOR_HEIGHT]}
+      scale={[GOAL_WIDTH, GOAL_DEPTH, 1]}
+      uniforms={{ color: 0xc0c0c0 }}
+    />
+    <!-- Goal Sides -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[GOAL_WIDTH, SOCCAR_YSIZE + GOAL_DEPTH, GOAL_HEIGHT]}
+      rotation={[0, 270, 0]}
+      scale={[GOAL_HEIGHT, GOAL_DEPTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    />
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[-GOAL_WIDTH, SOCCAR_YSIZE + GOAL_DEPTH, GOAL_HEIGHT]}
+      rotation={[0, 90, 0]}
+      scale={[GOAL_HEIGHT, GOAL_DEPTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    />
+    <!-- Goal Back -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[0, SOCCAR_YSIZE + (GOAL_DEPTH*2), GOAL_HEIGHT]}
+      rotation={[90, 90, 0]}
+      scale={[GOAL_HEIGHT, GOAL_WIDTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    />
+        <!-- Goal Top -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[0, SOCCAR_YSIZE + GOAL_DEPTH, GOAL_HEIGHT * 2]}
+      rotation={[0, 180, 90]}
+      scale={[GOAL_DEPTH, GOAL_WIDTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    />
+  </GL.Group>
+
+
+
   <!-- Front -->
   <GL.Mesh
     geometry={GL.plane()}
@@ -79,6 +124,50 @@
     uniforms={{ color: 0xffc0c0 }}
     transparent={true}
   />
+
+
+  <GL.Group>
+    <!-- Goal Floor -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[0, -SOCCAR_YSIZE - GOAL_DEPTH, FLOOR_HEIGHT]}
+      scale={[GOAL_WIDTH, GOAL_DEPTH, 1]}
+      uniforms={{ color: 0xc0c0c0 }}
+    /> 
+    <!-- Goal Sides -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[GOAL_WIDTH, -SOCCAR_YSIZE - GOAL_DEPTH, GOAL_HEIGHT]}
+      rotation={[0, 270, 0]}
+      scale={[GOAL_HEIGHT, GOAL_DEPTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    /> 
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[-GOAL_WIDTH, -SOCCAR_YSIZE - GOAL_DEPTH, GOAL_HEIGHT]}
+      rotation={[0, 90, 0]}
+      scale={[GOAL_HEIGHT, GOAL_DEPTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    />
+    <!-- Goal Back -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[0, -SOCCAR_YSIZE - (GOAL_DEPTH*2), GOAL_HEIGHT]}
+      rotation={[270, 270, 0]}
+      scale={[GOAL_HEIGHT, GOAL_WIDTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    />
+    <!-- Goal Top -->
+    <GL.Mesh
+      geometry={GL.plane()}
+      location={[0, -SOCCAR_YSIZE - GOAL_DEPTH, GOAL_HEIGHT * 2]}
+      rotation={[0, 180, 90]}
+      scale={[GOAL_DEPTH, GOAL_WIDTH, 1]}
+      uniforms={{ color: 0xffc0c0 }}
+    />
+  </GL.Group>
+
+
   <slot></slot>
 </GL.Group>
 
@@ -86,11 +175,13 @@
 <script>
   import * as GL from '@sveltejs/gl';
 
-  const SOCCAR_YSIZE = 10280 / 2
-  const SOCCAR_XSIZE = 8240 / 2
-  const SOCCAR_DEPTH = 1960 /2
-  const STADIUM_CORNER = 1e3
-  const GOAL_WIDTH = 1900 / 2 
-  const GOAL_HEIGHT = 800 / 2 
-  const GOAL_DEPTH = 900 / 2
+  const SOCCAR_YSIZE = 10280 / 2;
+  const SOCCAR_XSIZE = 8240 / 2;
+  const SOCCAR_DEPTH = 1960 /2;
+  const STADIUM_CORNER = 1e3;
+  const GOAL_WIDTH = 1900 / 2 ;
+  const GOAL_HEIGHT = 800 / 2 ;
+  const GOAL_DEPTH = 900 / 2;
+
+  const FLOOR_HEIGHT = 0;
 </script>
